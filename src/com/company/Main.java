772173c7a6309865bf84b5518e33fff4f6585c79
je565 <joshuaebyson@yahoo.com>
2017-10-16 08:50:52 -0400

    Not done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("Enter a word to be encrypted!");
+        EncryptWord abc = new EncryptWord("word");
+        System.out.println(abc.getEncryptIt());
     }
 }
