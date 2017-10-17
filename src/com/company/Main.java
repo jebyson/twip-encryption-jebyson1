9f711bb90577ca9a491616da27bb14309549b878
je565 <joshuaebyson@yahoo.com>
2017-10-17 08:43:56 -0400

    Done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,26 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Enter a word to be encrypted!");
-        EncryptWord abc = new EncryptWord("word");
-        System.out.println(abc.getEncryptIt());
+        EncryptWord extra = new EncryptWord("amazing");
+        System.out.println("The encryption is listed below:");
+        System.out.println(extra.encryptedWord());
+        System.out.println("Enter 'yes' if you would like to find the original message. Enter 'no' if you want to leave.");
+        Scanner input = new Scanner(System.in);
+        String yesNo = input.nextLine();
+
+        if(yesNo.equalsIgnoreCase("yes"))
+        {
+            System.out.print("Every letter in the encrypted word is two letters after the real letter. ");
+            System.out.print("Uppercase letters are encrypted by capital letters only. ");
+            System.out.print("Lowercase letters are encrypted by lowercase letters only. ");
+        }
+        else if(yesNo.equalsIgnoreCase("no"))
+        {
+            System.out.println("Thank you. Goodbye");
+        }
+        else
+        {
+            System.out.println("Invalid response. Goodbye");
+        }
     }
 }
